397d21f034399ee8f273db682d83f6532ee5dfbf
Neah Nieto <nrpcnieto@gmail.com>
2019-12-12 21:01:53 -0600

    Generate model/user.rb and db/migrate for Users model

---
+++ db/migrate/20191213030128_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :password_digest
+      t.boolean :is_mod
+      t.boolean :is_admin
+
+      t.timestamps
+    end
+  end
+end
