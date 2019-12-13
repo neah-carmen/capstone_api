0af5cee26899c326b16b7d0b9c624c3ec9e7dc58
Neah Nieto <nrpcnieto@gmail.com>
2019-12-12 21:00:19 -0600

    Generate model/edible.rb and db/migrate for Edibles model

---
+++ db/migrate/20191213025458_create_edibles.rb
@@ -0,0 +1,15 @@
+class CreateEdibles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :edibles do |t|
+      t.string :name
+      t.integer :upc
+      t.integer :user_id
+      t.integer :mod_id
+      t.boolean :verified
+      t.string :is_vegetarian
+      t.string :is_vegan
+
+      t.timestamps
+    end
+  end
+end
