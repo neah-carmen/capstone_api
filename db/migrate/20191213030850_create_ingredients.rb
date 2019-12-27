fb18e7ecf87a966b2c392ed150df1cdf866e9e6d
Neah Nieto <nrpcnieto@gmail.com>
2019-12-26 20:14:38 -0600

    Rename attribute for Ingredients model so that it mirrors the Edibles model

--- db/migrate/20191213030850_create_ingredients.rb
+++ db/migrate/20191213030850_create_ingredients.rb
@@ -2,8 +2,8 @@ class CreateIngredients < ActiveRecord::Migration[6.0]
   def change
     create_table :ingredients do |t|
       t.string :name
-      t.string :not_vegetarian
-      t.string :not_vegan
+      t.string :is_vegetarian
+      t.string :is_vegan
 
       t.timestamps
     end
