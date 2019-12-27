7f5f56d52e2b99454b280dc5c44b3770ec7d3d26
Neah Nieto <nrpcnieto@gmail.com>
2019-12-26 21:20:37 -0600

    Rewrite edibles_controller.rb so that the create action can receive multiple params for ingredients

--- app/controllers/api/edibles_controller.rb
+++ app/controllers/api/edibles_controller.rb
@@ -13,14 +13,16 @@ class Api::EdiblesController < ApplicationController
       user_id: current_user.id,
     )
     if @edible.save
-      ingredient = Ingredient.find_or_create_by(name: params[:ingredient_name], is_vegetarian: params[:is_vegetarian], is_vegan: params[:is_vegan])
-      if ingredient.save
-        food_label = FoodLabel.new(
-          edible_id: @edible.id,
-          ingredient_id: ingredient.id,
-        )
-        food_label.save
-      end
+      params[:ingredients].each { |ingredient_name|
+        ingredient = Ingredient.find_or_create_by(name: ingredient_name, is_vegetarian: params[:is_vegetarian], is_vegan: params[:is_vegan])
+        if ingredient.save
+          food_label = FoodLabel.new(
+            edible_id: @edible.id,
+            ingredient_id: ingredient.id,
+          )
+          food_label.save
+        end
+      }
       render "show.json.jb"
     end
   end
