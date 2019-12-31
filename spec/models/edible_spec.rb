dbe1fe12d8541dfd4408e5f97ee55f47dcb7ab59
Neah Nieto <nrpcnieto@gmail.com>
2019-12-31 11:20:44 -0600

    Add tests for is_vegan? method for Edibles model (reused code from is_vegetarian? spec)

--- spec/models/edible_spec.rb
+++ spec/models/edible_spec.rb
@@ -20,4 +20,24 @@ RSpec.describe Edible, type: :model do
       expect(test_edible.is_vegetarian).to eq("no")
     end
   end
+
+  describe "#is_vegan?" do
+    it "should set the value of is_vegan to 'yes' if all of the ingredients of an Edible are vegan" do
+      test_user = User.create(username: "test", email: "test@email.com", password: "password")
+      test_ingredient = Ingredient.create(name: "Corn Syrup", is_vegan: "yes")
+      test_edible = Edible.create(name: "test_vegan", user_id: test_user.id)
+      FoodLabel.create(edible_id: test_edible.id, ingredient_id: test_ingredient.id)
+      test_edible.is_vegan?
+      expect(test_edible.is_vegan).to eq("yes")
+    end
+
+    it "should set the value of is_vegan to 'no' if all of the ingredients of an Edible are vegan" do
+      test_user = User.create(username: "test", email: "test@email.com", password: "password")
+      test_ingredient = Ingredient.create(name: "Gelatin", is_vegan: "no")
+      test_edible = Edible.create(name: "not_vegan", user_id: test_user.id)
+      FoodLabel.create(edible_id: test_edible.id, ingredient_id: test_ingredient.id)
+      test_edible.is_vegan?
+      expect(test_edible.is_vegan).to eq("no")
+    end
+  end
 end
