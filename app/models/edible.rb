75ed36e79aff019eeb1ddd6c2d39efa79178ee2e
Neah Nieto <nrpcnieto@gmail.com>
2019-12-31 09:46:27 -0600

    Add skeleton code for is_vegetarian? method to Edibles model

--- app/models/edible.rb
+++ app/models/edible.rb
@@ -3,4 +3,22 @@ class Edible < ApplicationRecord
 
   has_many :food_labels
   has_many :ingredients, through: :food_labels
+
+  def is_vegetarian?
+    check = nil
+    ingredients_list = self.ingredients
+    ingredients_list.each do |ingredient|
+      if !ingredient.is_vegetarian
+        check = false
+        break
+      else
+        check = true
+      end
+    end
+    if check == true
+      puts "is vegetarian"
+    else
+      puts "not vegetarian"
+    end
+  end
 end
