b63fcfe93367cd14b9cd107126de0037ec8c1bb9
Neah Nieto <nrpcnieto@gmail.com>
2019-12-31 14:49:16 -0600

    Add and fix test for is_vegetarian parameter for format_params method in ingredient_spec.rb

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -2,9 +2,16 @@ require "rails_helper"
 
 RSpec.describe Ingredient, type: :model do
   describe "#format_params" do
-    it "should format the values for #is_vegetarian so that it is 'yes'" do
-      test_ingredient = Ingredient.create(name: "test_ingredient", is_vegetarian: "YES")
+    it "should format the value for #is_vegetarian so that any casing for the parameter 'yes' is saved as 'yes'" do
+      test_ingredient = Ingredient.create(name: "test_ingredient")
+      test_ingredient.format_params("YES", "yes")
       expect(test_ingredient.is_vegetarian).to eq("yes")
+    end
+
+    it "should format the value for #is_vegetarian so that any casing for the parameter 'no' is saved as 'no'" do
+      test_ingredient = Ingredient.create(name: "test_ingredient")
+      test_ingredient.format_params("NO", "no")
+      expect(test_ingredient.is_vegetarian).to eq("no")
     end
   end
 end
