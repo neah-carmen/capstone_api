a31f44bbe84b08126bee90d4b086b9e6dd0bb507
Neah Nieto <nrpcnieto@gmail.com>
2019-12-31 12:46:36 -0600

    Add test to check email validation for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#user_id" do
+    it "should validate that a user cannot be created without an email address" do
+      test_user = User.create(email: nil)
+      expect(test_user.id).to eq(nil)
+    end
+  end
 end
