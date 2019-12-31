18f3f27d16e86b244d2ad34e19a7be84abbec3b1
Neah Nieto <nrpcnieto@gmail.com>
2019-12-31 12:49:54 -0600

    Add test to check password validation for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,16 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  describe "#user_id" do
-    it "should validate that a user cannot be created without an email address" do
+  describe "#email" do
+    it "should validate that a new User cannot be created without an #email" do
       test_user = User.create(email: nil)
+      expect(test_user.id).to eq(nil)
+    end
+  end
+
+  describe "#password" do
+    it "should validate that a new User cannot be created without a #password" do
+      test_user = User.create(email: "test@email.com", password: nil)
       expect(test_user.id).to eq(nil)
     end
   end
