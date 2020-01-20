e1441ccd1e5af2ef4b09240e61fcb1551fa96d6d
Neah Nieto <nrpcnieto@gmail.com>
2020-01-19 23:46:28 -0600

    Add model methods to the edible create action (check for vegetarian and vegan attributes)

--- app/controllers/api/edibles_controller.rb
+++ app/controllers/api/edibles_controller.rb
@@ -28,6 +28,8 @@ class Api::EdiblesController < ApplicationController
           food_label.save
         end
       }
+      @edible.is_vegetarian?
+      @edible.is_vegan?
       render "show.json.jb"
     end
   end
