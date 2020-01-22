472c58663612c4af0e2e99ba164686c739188f03
Neah Nieto <nrpcnieto@gmail.com>
2020-01-21 20:57:28 -0600

    Add final part to label_reader controller (SQL query to search for existing ingredients)

--- app/controllers/api/label_reader_controller.rb
+++ app/controllers/api/label_reader_controller.rb
@@ -2,15 +2,15 @@ class Api::LabelReaderController < ApplicationController
   require "rtesseract"
 
   def show
-    new_scan = RTesseract.new("https://res.cloudinary.com/neah-dev/image/upload/v1579469493/iz8op9se6py9ppxypqqd.png")
-    # response = Cloudinary::Uploader.upload(params[:image])
-    # @cloudinary_url = response["secure_url"]
-    # new_scan = RTesseract.new(@cloudinary_url)
+    response = Cloudinary::Uploader.upload(params[:image])
+    @cloudinary_url = response["secure_url"]
+    new_scan = RTesseract.new(@cloudinary_url)
 
     label_extract = new_scan.to_s
     label_extract = label_extract[13..-1]
 
     @label_lines = []
+    @ingredients_extract = []
     @ingredients = []
 
     label_extract.split("\n").each do |line|
@@ -19,11 +19,20 @@ class Api::LabelReaderController < ApplicationController
     @label_lines.delete("\f")
 
     label_extract.split(/\s*([,;\r\n]+|\s\s)\s*/).each do |ingredient|
-      @ingredients << ingredient
+      @ingredients_extract << ingredient
     end
-    @ingredients.delete(",")
-    @ingredients.delete("\n")
-    @ingredients.delete(",\n")
+    @ingredients_extract.delete(",")
+    @ingredients_extract.delete("\n")
+    @ingredients_extract.delete(",\n")
+
+    @ingredients_extract.each do |ingredient_name|
+      ingredient = Ingredient.where("lower(name) = ?", ingredient_name.downcase).first
+      if ingredient
+        @ingredients << { name: ingredient.name, is_vegan: ingredient.is_vegan, is_vegetarian: ingredient.is_vegetarian }
+      else
+        @ingredients << { name: ingredient_name, is_vegan: nil, is_vegetarian: nil }
+      end
+    end
 
     render "show.json.jb"
   end
